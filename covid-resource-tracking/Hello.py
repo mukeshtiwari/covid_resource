0ac8968beb2801438c5be9a0051e6aa471925b6c
mukesh tiwari <mukeshtiwari.iiitm@gmail.com>
2021-04-24 17:49:24 +1000

    added two url to handle city and pincode

--- covid-resource-tracking/Hello.py
+++ covid-resource-tracking/Hello.py
@@ -1,9 +1,22 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return 'Hello World Mukesh Tiwari'
+
+#this function takes picode and 
+#returns the list of suppliers
+@app.route('/pincode/<string:pcode>')
+def fetch_data_by_pincode(pcode):
+  return 'Pincode is %s' % pcode
+
+#this function takes city and 
+#returns the list of suppliers
+@app.route('/city/<string:name>')
+def fetch_data_by_city(name):
+  return 'city is %s' % name
+
+@app.route('/hello/<name>')
+def hello_world(name):
+  return 'Hello %s' % name 
 
 if __name__ == '__main__':
    app.run()
